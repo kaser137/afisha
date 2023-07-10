7048489418adf8dd4ce43a6df2182e82169c50f0
kaser137 <murilon375@gmail.com>
2023-07-10 06:27:19 +0300

    Setup media files serving

--- afisha/urls.py
+++ afisha/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
+
+
 from django.urls import path, include
+
 
 from places.views import index
 
@@ -23,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('city', include('places.urls')),
     path('', index),
-]
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
