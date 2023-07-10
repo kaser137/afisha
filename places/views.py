a495963a9c8d67eaf81717ab9e5b99df0b5dcd8e
kaser137 <murilon375@gmail.com>
2023-07-10 09:12:24 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -3,5 +3,32 @@ from django.shortcuts import render
 
 
 def index(request):
+    st1 = "{% static 'static/moscow_legends.json' %}"
+    payload = {"points": {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": dict(title="«Легенды Москвы", placeId="moscow_legends",
+                                   detailsUrl="static/moscow_legends.json")
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "static/roofs24.json"
+                }
+            }
+        ]
+    }}
 
-    return render(request, 'places/index.html')
+    return render(request, 'places/index.html', context=payload)
