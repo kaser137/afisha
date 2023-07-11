4e9632ef6ee0140bed76d362df6f4c8e175e3163
kaser137 <murilon375@gmail.com>
2023-07-11 05:14:33 +0300

    create inline form

--- places/admin.py
+++ places/admin.py
@@ -5,6 +5,17 @@ from places.models import Place, Image
 # admin.site.register(Place)
 admin.site.register(Image)
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    raw_id_fields = ('place',)
+    extra = 1
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title', 'id']
+    inlines = [ ImageInline, ]
+
+
+
